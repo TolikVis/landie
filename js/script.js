2c389421e0849bc437c9a478cc8726c35dc6277c
Користувач <visockijtolik@gmail.com>
2021-03-31 11:50:41 +0300

    init work

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let iconMenu = document.querySelector('.menu__icon');
+let menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
